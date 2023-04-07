dc02daab8f3ffc054f127ee9764cb8d78204e10f
Steve Nogar <stephen.m.nogar.civ@army.mil>
2023-04-07 11:50:37 -0400

    hardcode honda insight fingerprint

--- launch_env.sh
+++ launch_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+export FINGERPRINT="HONDA INSIGHT 2019"
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+export FINGERPRINT="HONDA INSIGHT 2019"
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
